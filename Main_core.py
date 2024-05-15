df11d67e6b77959859bbe4e44977443243a5ef01
Mario <mario.pba@gmail.com>
2024-05-15 10:02:53 +0000

    Update Ollama model temperature in Main_core.py

--- Main_core.py
+++ Main_core.py
@@ -9,52 +9,58 @@ from dotenv import load_dotenv
 load_dotenv()
 
 groq = ChatGroq(temperature=0.0, model_name="llama3-8b-8192") # mixtral-8x7b-32768 - llama3-70b-8192 - gemma-7b-it - llama3-8b-8192
-gpt35_turbo = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
+gpt35_turbo = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
 llm = groq
 
 search_tool = SerperDevTool()
 #question = input("What is your question? ")
-question  = "How to solve the Ukranian war?"
+question  = "How to solve the Israel - Palestin war?"
 
 # Define the agents with their specific roles and goals
-melchior = Agent(
+Scientist = Agent(
     role='Scientist',
     goal='Conduct technical analysis and provide logical conclusions.',
-    backstory='Melchior is a renowned scientist with expertise in data analysis and interpretation in war zones.',
-    #memory=True,
-    verbose=True,
-    allow_delegation=True,
-    llm = llm
-)
-
-balthasar = Agent(
-    role='Strategist',
-    goal='Develop defense strategies and oversee tactical operations.',
-    backstory='Balthasar is a seasoned strategist with a deep understanding of military tactics and security in conflict zone.',
-    #memory=True,
+    backstory='Melchior is a renowned scientist with expertise in data analysis and interpretation in war zones\
+        he analyzes the history of the conflicts and both parties involved.',
+    memory=True,
     verbose=True,
     allow_delegation=True,
     llm = llm,
+    tools = [search_tool]
+)
+
+Strategist = Agent(
+    role='Strategist',
+    goal='Develop defense strategies and oversee tactical operations.',
+    backstory='Balthasar is a seasoned strategist with a deep understanding of military tactics and security in conflict zone\
+        he analyzes the situation and finds a peacful solution.',
+    #memory=True,
+    verbose=True,
+    allow_delegation=False,
+    llm = llm,
+    tools = [search_tool],
     output_file='Strategist.txt'
 )
 
-caspar = Agent(
+Diplomat = Agent(
     role='Diplomat',
     goal='Evaluate ethical implications and make balanced decisions.',
-    backstory='Caspar is a skilled diplomat with a keen sense of ethics and morality, he knows very well the history of the parts involved',
+    backstory='Caspar is a skilled diplomat with a keen sense of ethics and morality, he knows very well the history of the parts involved\
+        he seeks always a peaceful solution.',
     #memory=True,
     verbose=True,
-    allow_delegation=True,
+    allow_delegation=False,
+    tools = [search_tool],
     llm = llm
 )
 
-reporter = Agent(
+Reporter = Agent(
     role='Reporter',
     goal='gather the information from the previous analysis and make an action plan',
     backstory='Reporter is able to gather all the information from previuos agents and propose an internationl threat that can be accepted by all the parts in conflict',
     #memory=True,
     verbose=True,
-    allow_delegation=True,
+    allow_delegation=False,
     llm = llm
 )
 
@@ -64,38 +70,39 @@ reporter = Agent(
 scientific_analysis_task = Task(
     description=f'Analyze the technical data provided and extract conclusions about {question}.',
     expected_output='Detailed report with conclusions based on the data analysis.',
-    agent = melchior,
-    tools = [search_tool]
+    agent = Scientist,
+    
 )
 
 strategy_task = Task(
     description='Formulate a strategic plan based on current threats.',
-    expected_output='A comprehensive defense strategy.',
-    agent = balthasar
+    expected_output='A comprehensive peace strategy.',
+    agent = Strategist
 )
 
 diplomacy_task = Task(
     description='Assess the ethical implications of a proposed action.',
     expected_output='A reasoned judgment on the ethical acceptability of the action.',
-    agent = caspar
+    agent = Diplomat,
+    human_input=True
 )
 
 action_plan_task = Task(
     description='Gather the information from the previous analysis and make an action plan',
     expected_output='A detailed action plan based on the analysis and conclusions.',
-    agent = reporter
+    agent = Reporter
 )
 # Form the crew
-magi_system = Crew(
-    agents=[melchior, balthasar, caspar, reporter],
+paxai_system = Crew(
+    agents=[Scientist, Strategist, Diplomat, Reporter],
     tasks=[scientific_analysis_task, strategy_task, diplomacy_task, action_plan_task],
     #memory=True,
     cache=True,
-    verbose=1
-    #process=Process.sequential  # assuming a consensus process is needed for decision
+    verbose=1,
+    process=Process.sequential  # assuming a consensus process is needed for decision
 )
 
-result = magi_system.kickoff()
+result = paxai_system.kickoff()
 print("######################")
 print(result)
 # genrate a text file with the question and the result
@@ -103,4 +110,4 @@ with open(f"PaxAI_response.txt", "w") as f:
     f.write(f"Question: {question}\n")
     f.write(f"Answer: {result}")
 
-print(magi_system.usage_metrics)
+print(paxai_system.usage_metrics)
